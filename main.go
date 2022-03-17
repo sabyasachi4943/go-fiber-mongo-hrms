8d27d22cf518c57b1345f7f67747ef292711e66d
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-03-17 22:41:13 +0600

    post method added

--- main.go
+++ main.go
@@ -6,13 +6,14 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type MongoInstance struct {
 	Client *mongo.Client
-	Db		 *mongo.Database
+	Db     *mongo.Database
 }
 
 var mg MongoInstance
@@ -21,10 +22,10 @@ const dbName = "fiber-hrms"
 const mongoURI = "mongodb://localhost:27017" + dbName
 
 type Employee struct {
-	ID     string			`json:"id, omitempty" bson:"_id, omitempty"`
-	Nmae   string			`json:"name"`
-	Salary float64		`json:"salary"`
-	Age    float64		`json:"age"`
+	ID     string  `json:"id, omitempty" bson:"_id, omitempty"`
+	Nmae   string  `json:"name"`
+	Salary float64 `json:"salary"`
+	Age    float64 `json:"age"`
 }
 
 func Connect() error {
@@ -41,7 +42,7 @@ func Connect() error {
 
 	mg = MongoInstance{
 		Client: client,
-		Db: db,
+		Db:     db,
 	}
 	return nil
 }
@@ -56,9 +57,50 @@ func main() {
 
 	app.Get("/employee", func(c *fiber.Ctx) error {
 
+		query := bson.D{{}}
+
+		cursor, err := mg.Db.Collection("employees").Find(c.Context(), query)
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		var employees []Employee = make([]Employee, 0)
+
+		if err := cursor.All(c.Context(), &employees); err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+
+		return c.JSON(employees)
 	})
-	app.Post("/employee")
+	app.Post("/employee", func(c *fiber.Ctx) error {
+		collection := mg.Db.Collection("employees")
+
+		employee := new(Employee)
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		employee.ID = ""
+
+		insertionResult, err := collection.InsertOne(c.Context(), employee)
+
+		if err != nil {
+			return c.Status(500).SendString(err.Error())
+		}
+		
+		filter := bson.D{{Key: "_id", Value: insertionResult.InsertedID}}
+		createdRecord := collection.FindOne(c.Context(), filter)
+
+		createdEmployee := &Employee{}
+		createdRecord.Decode(createdEmployee)
+
+		return c.Status(201).JSON(createdEmployee)
+	})
+
+
 	app.Put("/employee/:id")
+
+
+	
 	app.Delete("/employee/:id")
 
 }
