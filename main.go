5fb662b05a8752c842cc7824c4b464c51793b6c7
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-03-17 23:23:28 +0600

    added put method

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -97,10 +98,47 @@ func main() {
 	})
 
 
-	app.Put("/employee/:id")
+	app.Put("/employee/:id", func(c *fiber.Ctx) error {
+		idParam := c.Params("id")
+
+		employeeID, err := primitive.ObjectIDFromHex(idParam)
+
+		if err != nil {
+			return c.SendStatus(400)
+		}
+		employee := new(Employee)
+
+		if err := c.BodyParser(employee); err != nil {
+			return c.Status(400).SendString(err.Error())	
+		}
+
+		query := bson.D{{Key:"_id", Value: employeeID}}
+		update := bson.D{
+			{Key: "$set",
+				Value: bson.D{
+					{Key: "name", Value: employee.Name},
+					{Key: "age", Value: employee.Age},
+					{Key: "salary", Value: employee.Salary},
+				},
+			},
+		}
+
+		err = mg.Db.Collection("employees").FindOneAndUpdate(c.Context(), query, update).Err()
+
+		if err != nil {
+			if err == mongo.ErrNoDocuments{
+				return c.SendStatus(400)
+			}
+			return c.SendStatus(500)
+		}
+
+		employee.ID = idParam
+
+		return c.Status(200).JSON(employee)
+	})
 
 
-	
+	 
 	app.Delete("/employee/:id")
 
 }
