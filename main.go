e328b8b7a4025f09ebe80226cafff13a2caa0bfb
Sabyasachi Sarkar <54597679+sabyasachi4943@users.noreply.github.com>
2022-03-20 18:58:23 +0600

    delete function implemended

--- main.go
+++ main.go
@@ -139,6 +139,28 @@ func main() {
 
 
 	 
-	app.Delete("/employee/:id")
+	app.Delete("/employee/:id", func(c *fiber.Ctx) error {
+		eployeeID, err := primitive.ObjectIDFromHex(c.Params("id"),)
+
+		if err != nil {
+			return c.SendStatus(400)
+		}
+
+		query := bson.D{{Key:"_id", Value: employeeID}}
+		result, err := mg.Db.Collection("employees").DeleteOne(c.Context(), &query)
+
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		if result.DeletedCount < 1 {
+			return c.SendStatus(404)
+		}
+
+		return c.Status(200).JSON("record deleted")
+		
+	})
+
+	log.Fatal(app.Listen(":3000"))
 
 }
